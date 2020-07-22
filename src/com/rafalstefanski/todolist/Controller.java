2f47edec0739643b15be16d5fe5e15791d1ceeab
Rafał Stefański <rafal-stefanski@wp.pl>
2020-07-22 18:20:08 +0200

    Added menu with New.. button.

--- src/com/rafalstefanski/todolist/Controller.java
+++ src/com/rafalstefanski/todolist/Controller.java
@@ -9,6 +9,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -133,6 +135,15 @@ public class Controller {
     }
 
     @FXML
+    public void handleKeyPressed(KeyEvent keyEvent) {
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            if (keyEvent.getCode().equals(KeyCode.DELETE)) {
+                deleteItem(selectedItem);
+            }
+        }
+    }
+    @FXML
     public void handleClickListView() {
         TodoItem item = todoListView.getSelectionModel().getSelectedItem();
         itemDetailsTextArea.setText(item.getDetails());
@@ -150,6 +161,7 @@ public class Controller {
             TodoData.getInstance().deleteTodoItem(item);
         }
     }
+
 }
 
 
